24195271e951215063ec7cfe1fb1ace10c3f6f8a
unknown <saraazabel@gmail.com>
2021-03-23 11:56:31 -0300

    make it better

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const port = 3000
 
 app.use('/', routes);
 
-app.get('*', (req, res) => {
-  res.send('Hello World!')
+// app.get('*', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+app.get("/users/:user_id", (req, resp) => {
+  resp.send("Voce consultou o" + req.params.user_id)
 })
 
 app.listen(port, () => {
---
+++ server/db/config/index.js
@@ -0,0 +1,3 @@
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('https://git.heroku.com/api-bburger.git')
